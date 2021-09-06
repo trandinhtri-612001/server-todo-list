6f23047d41517b7c60a81598e2733595b6bdda3a
unknown <trantri123rc@gmail.com>
2021-09-06 09:59:04 +0700

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const ConnectDB = require('./connectdb/ConnectDB');
+const mongoose = require ('mongoose')
+
 
 const cors = require ('cors')
  
+const ConnectDB = async ()=>{
+
+    try {
+		await mongoose.connect(`mongodb+srv://todo_list:${process.env.password}@cluster0.oa0wn.mongodb.net/TodoListData?retryWrites=true&w=majority`,
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				
+				
+			}
+		)
+
+		console.log('MongoDB connected')
+	} catch (error) {
+		console.log(error)
+
+	}
+}
+
 
 
 
@@ -29,6 +49,6 @@ app.use(cors())
 app.use('/api/auth',UsersRouter)
 app.use('/api/post', PostsRouter)
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
